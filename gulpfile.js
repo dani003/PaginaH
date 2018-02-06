489ad81383c8283163afd1e6668cba553160318d
dani003 <daniela.ramirez003@gmail.com>
2018-02-05 23:17:37 -0300

    1°Act

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp =require('gulp');
+const pug = require('gulp-pug');
+const stylus = require('gulp-stylus');
+const browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+gulp.task('styles', function(){
+  gulp.src('./stylus/*.styl')
+  .pipe(stylus())
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function buildHtml(){
+  return gulp.src('./pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    server:"./"
+  })
+});
+
+gulp.task('watch', ["browser-sync", "pug", "styles"]), () =>{
+  gulp.watch("./pug/*.pug", ['pug']);
+  gulp.watch("./stylus/*.styl", ['styles']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+gulp.task('default', ["browser-sync", "pug", "watch", "styles"]);
