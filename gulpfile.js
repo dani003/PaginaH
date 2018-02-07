d4f60386d94333fa52ff94ce148eb4f609465693
dani003 <daniela.ramirez003@gmail.com>
2018-02-07 09:10:44 -0300

    dn

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,6 @@ const browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 
-gulp.task('serve', ['pug', 'styles'], function(){
-  browserSync.init({
-    server:"./"
-  })
-  gulp.watch("./pug/index.pug", ['pug']);
-  gulp.watch("./pug/blocks/*.pug", ['pug']);
-  gulp.watch("./stylus/*.styl", ['styles']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
-
 gulp.task('styles', function(){
   gulp.src('./stylus/*.styl')
   .pipe(stylus())
@@ -31,5 +21,14 @@ gulp.task('pug', function buildHtml(){
   .pipe(browserSync.stream());
 });
 
+gulp.task('serve', ['pug', 'styles'], function(){
+  browserSync.init({
+    server:"./"
+  })
+  gulp.watch("./pug/index.pug", ['pug']);
+  gulp.watch("./pug/blocks/*.pug", ['pug']);
+  gulp.watch("./stylus/*.styl", ['styles']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
 
-gulp.task('default', ["serve"]);
+gulp.task('default', ["serve", "pug", "styles"]);
